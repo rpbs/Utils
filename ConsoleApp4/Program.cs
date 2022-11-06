7d0894a4f946ce7e91e92e8f3b53a246c5866fdc
renyo.borges <renyo.borges@hrmsolucoes.com.br>
2022-11-06 20:35:48 -0300

    ooo

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,76 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp4;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
-
-
-
-IDictionary<string, string> test = new Dictionary<string, string>
-{
-    ["a"] = "a",
-
-    ["b"] = "b"
-};
 
 string pattern = @"<body(>|.*)>((.|\n)*)</body>";
 // Create a Regex  
 Regex rg = new(pattern);
 
 // Long string  
-string authors = File.ReadAllText("test.txt");
+string authors =  File.ReadAllText("test.txt");
 // Get all matches  
 MatchCollection matchedAuthors = rg.Matches(authors);
 
 int found = 0;
 
-var asd = matchedAuthors[0].Value;
+var body = matchedAuthors[0].Value.Replace("\r", "").Replace("\n", "");
 
-NewMethod(ref asd, ref found);
+NewMethod(ref body, ref found);
+
+Console.WriteLine(body.TrimStart().TrimEnd());
 
 static void NewMethod(ref string bodyMatch, ref int found)
 {
-    if (found == 3)
-    {
-        Console.WriteLine(bodyMatch);
-
-        return;
-    }
-    var body = "";
     if (bodyMatch is not null)
-    {        
+    {
         foreach (var tag in HtmlTags.Tags)
         {
-            var rg = new Regex($"<{tag}(>|.*)>((.|\n)*)</{tag}>");
-            var matchedAuthors = rg.Matches(bodyMatch);
-
-            for (int count = 0; count < matchedAuthors.Count; count++)
+            var rg = new Regex($"<{tag}\\s.+?\">");
+            if (rg.IsMatch(bodyMatch))
             {
-                body += matchedAuthors[count].Value
-                    .Replace($"<{tag}>", "")
-                    .Replace($"</{tag}>", "")
-                    .Replace("\r", "")
-                    .Replace("\n", "");
-                // remover all tags here at body
-                rg = new Regex($"<{tag}\\s.+?\">");
-                if (rg.IsMatch(body))
-                {
-                    bodyMatch = rg.Replace(body, "");
-                    found++;
-                    NewMethod(ref bodyMatch, ref found);
-                }
-                rg = new Regex($"<//{tag}>");
-                if (rg.IsMatch(body))
-                {
-                    bodyMatch = rg.Replace(body, "");
-                    found++;
-                    NewMethod(ref bodyMatch, ref found);
-                }
-                 
-                rg = new Regex("");
-
-            }            
+                bodyMatch = rg.Replace(bodyMatch, "");
+                found++;
+            }
+            rg = new Regex($"<{tag}>");
+            if (rg.IsMatch(bodyMatch))
+            {
+                bodyMatch = rg.Replace(bodyMatch, "");
+                found++;
+            }
+            rg = new Regex(@$"</{tag}>");
+            if (rg.IsMatch(bodyMatch))
+            {
+                bodyMatch = rg.Replace(bodyMatch, "");
+                found++;
+            }
         }
-        
     }
 }
 
